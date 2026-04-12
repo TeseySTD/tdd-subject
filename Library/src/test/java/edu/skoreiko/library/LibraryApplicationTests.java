3df22224e9343ffd788ada4c138054d56eb1e4e8
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2026-04-12 13:45:53 +0300

    feat(lab2):Make sandbox.

---
+++ Library/src/test/java/edu/skoreiko/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.skoreiko.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
